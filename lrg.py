ee0d3ab7d5617aa72b766182e3d3ae21603e0fed
Andy <andrew.parrish@nhs.net>
2014-11-14 04:05:35 -0800

    Added assertion checks and try/excepts for file inputs

--- lrg.py
+++ lrg.py
@@ -1,10 +1,11 @@
 # Import required modules
 
-import xml.etree.ElementTree as et, argparse
+import xml.etree.ElementTree as et, argparse, sys
 
 # Get parameters
 
 def main():
+    '''Main function that collects command line arguments and assigns to global variables'''
     global lrg_file, fasta, tree, root, include_introns
     parser = argparse.ArgumentParser()
     parser.add_argument('LRG_file',  help = 'Input LRG filename')
@@ -20,38 +21,48 @@ def main():
     else:
         include_introns = False
 
-    tree = et.parse(lrg_file)
+    try:
+        tree = et.parse(lrg_file) 
+    except:
+        print 'Unable to parse/open file'
+        sys.exit()
+        
     root = tree.getroot()
-
-# Get element text function
+    assert root.tag == 'lrg', 'Input file is not a valid LRG file'
 
 def get_element_text(value):
+    '''Returns text of selected element'''
     for element in root.findall(value):
+        assert len(element) > 0, 'Element has no associated text' 
         return element.text
 
-# Pull out sequence identifiers to write into fasta headers
-
 def get_file_info(tree):
+    '''Creates fasta headers to write into output file'''
     lrg_id = get_element_text('./fixed_annotation/id')
     hgnc_id = get_element_text('./fixed_annotation/hgnc_id')
     file_id = 'LRG ID:' + lrg_id + ' HGNC ID:' + hgnc_id + ' Exon: '
     return file_id
 
-# Get exon coordinates
-
 def get_exon_info(path, value):
+    '''Gets coordinates of exons'''
     exon_dict = {}
     for element in root.findall(path):
+        assert len(element[0].attrib) > 0, 'Exon is missing start/end coordinates'
         exon_dict[element.get(value).zfill(3)] = element[0].attrib
+    assert len(exon_dict) > 0, 'No Exons found in file'
     return exon_dict
 
-# Get fasta format
-
 def get_fasta(exon_dict,file_info):
+    '''Collates sequences and outputs fasta files'''
     file = open(fasta,'w')
     for exon in sorted(exon_dict):
-            start = int(exon_dict[exon]['start'])
-            end = int(exon_dict[exon]['end'])
+            try:
+                start = int(exon_dict[exon]['start'])
+                end = int(exon_dict[exon]['end'])
+            except KeyError:
+                print 'Start/End Coordinates not present for at least one exon'
+                sys.exit()
+
             if include_introns == False:
                 file.write('>' + get_file_info(tree) + exon + '\n')
                 file.write(ref_seq[start-1: end-1] + '\n')
@@ -68,9 +79,3 @@ lrg_info = get_file_info(root)
 exon_info = get_exon_info('./fixed_annotation/transcript/exon', 'label')
 fasta = get_fasta(exon_info,lrg_info)
 
-# Capture transcripts
-
-#def get_transcripts(path, value):
-#   for element in root.findall(path):
-#       transcripts.append(element.get(value))
-#    return transcripts
