0ce9ce967bd0c0001cd7632ea7954e1941ccbf3a
Andy <andrew.parrish@nhs.net>
2014-11-14 01:56:33 -0800

    Updated params

--- input_params.py
+++ input_params.py
@@ -3,15 +3,19 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('LRG_file', help = 'Input LRG filename')
-    parser.add_argument('Output filename', help = 'Output filename') 
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('-a', help = 'Adds numbers together', action='store_true')
-    group.add_argument('-s', help = 'Subtracts second number from first', action='store_true')
+    parser.add_argument('Out_file', help = 'Output filename') 
+    parser.add_argument('-i', help = 'Include flanking intronic regions', nargs = 2, action = 'store_false', default = [0,0])
 
-	args = parser.parse_args()
-	if args.a:
-		answer = args.first + args.second
-	elif args.s:
-		answer = args.first - args.second
+    args = parser.parse_args()
+    lrg_filename = args.LRG_file
+    outfile_name = args.Out_file   
+    if -i:
+        minus, plus  = args.i
+
+    print lrg_filename	
+    print outfile_name
+    print minus
+    print plus
+    
 
 main()
