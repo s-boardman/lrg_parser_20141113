3b02885aa3a3ce857a00aa79bc157062432dd039
Andy <andrew.parrish@nhs.net>
2014-11-14 02:17:49 -0800

    Added input parameters (may have broken it in the process!)

--- lrg.py
+++ lrg.py
@@ -1,11 +1,26 @@
 # Import required modules
 
-import xml.etree.ElementTree as et
+import xml.etree.ElementTree as et, argparse
 
-# Import file and get root
+# Get parameters
 
-tree = et.parse('LRG_1.xml')
-root = tree.getroot()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('LRG_file',  help = 'Input LRG filename')
+    parser.add_argument('Out_file', help = 'Output fasta filename')
+    parser.add_argument('-i', help = 'Includes 100bp on either side of exon', action = 'store_true')
+
+    args = parser.parse_args()
+    lrg_file = args.LRG_file
+    fasta = args.Out_file
+
+    if args.i:
+        include_introns = True
+    else:
+        include_introns = False
+
+    tree = et.parse(lrg_file)
+    root = tree.getroot()
 
 # Get element text function
 
@@ -24,24 +39,24 @@ def get_exon_info(path, value):
 # Get fasta format
 
 def get_fasta(exon_dict):
-    file = open('LRG_1.fa','w')
+    file = open(fasta,'w')
     for exon in sorted(exon_dict):
-        start = int(exon_dict[exon]['start'])
-        end = int(exon_dict[exon]['end'])
+        if include_introns == True:
+            start = int(exon_dict[exon]['start']) - 50
+            end = int(exon_dict[exon]['end']) + 50
+        else:
+            start = int(exon_dict[exon]['start'])
+            end = int(exon_dict[exon]['end'])
         file.write('>' + exon + '\n')
         file.write(ref_seq[start-1: end-1] + '\n')
     file.close()
 
+# Call main to get inputs
+main()
 # Call functions to produce output
 ref_seq = get_element_text('./fixed_annotation/sequence')
 exon_info = get_exon_info('./fixed_annotation/transcript/exon', 'label')
 fasta = get_fasta(exon_info)
-
-
-
-
-
-
 
 # Capture transcripts
 
